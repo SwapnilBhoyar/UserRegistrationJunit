f4cb251b02cc4363102f87366855115c26711885
SwapnilBhoyar <swapnilbhoyar95@gmail.com>
2021-05-07 17:58:31 +0530

    multiple emails

--- src/main/java/ValidateUser.java
+++ src/main/java/ValidateUser.java
@@ -1,12 +1,13 @@
 import java.util.regex.Pattern;
+import java.util.ArrayList;
 
 public class ValidateUser {
     private static final String FIRST_NAME_PATTERN = "^[A-Z]{1}[a-z]{3,}$";
     private static final String LAST_NAME_PATTERN = "^[A-Z]{1}[a-z]{3,}$";
-    private static final String EMAIL_PATTERN= "[0-9 a-z A-z]+([._+-][0-9 a-z A-Z]+)*"+"@([0-9 a-z A-Z][-]?)+[.][a-z A-Z]{2,4}([.][a-z A-Z]{2,4})?$";
+    private static final String EMAIL_PATTERN = "[0-9 a-z A-z]+([._+-][0-9 a-z A-Z]+)*" + "@([0-9 a-z A-Z][-]?)+[.][a-z A-Z]{2,4}([.][a-z A-Z]{2,4})?$";
     private static final String MOB_NUMBER = "^[1-9]{2}\\s{0,1}[0-9]{5}[0-9]{5}$";
     private static final String PASSWORDRULE1 = "[a-z]{8}";
-    private static final String PASSWORDRULE2 =  "[a-z A-Z]{8}";
+    private static final String PASSWORDRULE2 = "[a-z A-Z]{8}";
     private static final String PASSWORDRULE3 = "[0-9 a-z A-Z]{8}";
     private static final String PASSWORDRULE4 = "^(?=.?[A-Z])(?=(.[a-z]){1,})(?=[.]{1,})(?=1,})(?!.*\\s).{8,}$";
 
@@ -69,7 +70,7 @@ public class ValidateUser {
     }
 
     public static String validateemail(String email) {
-        String emailnamepattern = "[0-9 a-z A-z]+([._+-][0-9 a-z A-Z]+)*"+"@([0-9 a-z A-Z][-]?)+[.][a-z A-Z]{2,4}([.][a-z A-Z]{2,4})?$";
+        String emailnamepattern = "[0-9 a-z A-z]+([._+-][0-9 a-z A-Z]+)*" + "@([0-9 a-z A-Z][-]?)+[.][a-z A-Z]{2,4}([.][a-z A-Z]{2,4})?$";
         if (Pattern.matches(emailnamepattern, email))
             return "Happy";
         else
@@ -91,5 +92,32 @@ public class ValidateUser {
             return "Happy";
         else
             return "Sad";
+    }
+
+    public static void main(String[] args) {
+        ArrayList<String> emails = new ArrayList<String>();
+        emails.add("abc@yahoo.com");
+        emails.add("abc-100@yahoo.com");
+        emails.add("abc.100@yahoo.com");
+        emails.add("abc111@abc.com");
+        emails.add("abc-100@abc.com.net");
+        emails.add("abc.100@abc.com.au");
+        emails.add("abc@1.com");
+        emails.add("abc@gmail.com.com");
+        emails.add("abc+100@gmail.com");
+        emails.add("abc");
+        emails.add("abc@.com.my");
+        emails.add("abc123@gmail.a");
+        emails.add("abc123@.com");
+        emails.add("abc123@.com.com");
+        emailvalidation(emails);
+    }
+
+    private static void emailvalidation(ArrayList<String> emails) {
+        String emailPattern = "[0-9 a-z A-z]+([._+-][0-9 a-z A-Z]+)*\"+\"@([0-9 a-z A-Z][-]?)+[.][a-z A-Z]{2,4}([.][a-z A-Z]{2,4})?$";
+        for (String data : emails) {
+            if (Pattern.matches(emailPattern, data)) ;
+            System.out.println("Valid");
+        }
     }
 }
